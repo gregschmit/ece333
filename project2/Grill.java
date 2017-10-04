6c394219688cdcaedad38d2c7e48cc900f3d2f87
Gregory N. Schmit <schmitgreg@outlook.com>
2017-10-04 14:43:14 -0500

    gns: grill

---
+++ project2/Grill.java
@@ -0,0 +1,55 @@
+/* Grill, the mail agent made by Greg and Phil. */
+
+import java.io.*;
+import java.net.*;
+
+public class EmailSender
+{
+   public static void main(String[] args) throws Exception
+   {
+      // Establish a TCP connection with the mail server.
+
+
+      // Create a BufferedReader to read a line at a time.
+      InputStream is = socket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+
+      // Read greeting from the server.
+      String response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("220")) {
+         throw new Exception("220 reply not received from server.");
+      }
+
+      // Get a reference to the socket's output stream.
+      OutputStream os = socket.getOutputStream();
+
+      // Send HELO command and get server response.
+      String command = "HELO alice\r\n";
+      System.out.print(command);
+      os.write(command.getBytes("US-ASCII"));
+      response = br.readLine();
+      System.out.println(response);
+      if (!response.startsWith("250")) {
+         throw new Exception("250 reply not received from server.");
+      }
+
+      // Send MAIL FROM command.
+
+
+      // Send RCPT TO command.
+
+
+      // Send DATA command.
+
+
+      // Send message data.
+
+      // End with line with a single period.
+
+
+      // Send QUIT command.
+
+   }
+}
